7eb3a74a17ad57533b96ccb6dd7a5d04f7376d95
Marian <26277754+Majoouoo@users.noreply.github.com>
2024-12-01 15:16:39 +0100

    Added floating label to input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script>
-  let shadowX = 8;
-  let shadowY = 8;
+  let shadowX = 10;
+  let shadowY = 10;
   let shadowLength = 0;
   let cornerRadius = 8;
   let borderWidth = 2;
   let shadowColor = "#000";
   let elementColor = "#fff";
   let transitionLength = 0.2;
+  let fontSizeRem = 1;
 
   let boxShadow = "";
   for (let i = 1; i < Math.max(shadowX, shadowY); i++) {
@@ -26,6 +27,7 @@
   --shadow-color: {shadowColor};
   --element-color: {elementColor};
   --transition-length: {transitionLength}s;
+  --font-size-rem: {fontSizeRem}rem;
   --box-shadow: {boxShadow};
   ">
     <div id="settings">
@@ -37,7 +39,7 @@
         <h1>Button</h1>
       </header>
       <div class="component-wrapper">
-        <button class="component">Click</button>
+        <button class="component">Log in</button>
       </div>
     </div>
     <div class="code">
@@ -49,7 +51,10 @@
         <h1>Input</h1>
       </header>
       <div class="component-wrapper">
-        <input type="text" class="component">
+        <div class="input-wrapper">
+          <input type="text" name="Input" class="component" placeholder=" ">
+          <label for="Input" class="input-label">Username</label>
+        </div>
       </div>
     </div>
     <div class="code">
@@ -102,29 +107,58 @@
   }
 
   .component {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 1rem;
-    padding: 1rem 2rem;
+    padding: var(--font-size-rem) calc(var(--font-size-rem) * 2);
     font-family: "Inter", sans-serif;
-    font-size: 1rem;
+    font-size: var(--font-size-rem);
     border-radius: var(--corner-radius);
     border: var(--border-width) solid var(--shadow-color);
     box-shadow: var(--box-shadow);
     background-color: var(--element-color);
+    color: var(--shadow-color);
     transition-property: transform, box-shadow;
     transition-duration: var(--transition-length);
-  }
-
-  input.component {
-    padding: 1rem;
   }
 
   .component:active, input.component:focus {
     transform: translate(var(--shadow-x), var(--shadow-y));
     box-shadow: none;
     outline: none;
+  }
+
+  input.component {
+    padding: var(--font-size-rem);
+    position: relative;
+    grid-row: 1;
+    grid-column: 1;
+  }
+
+  .input-label {
+    font-size: var(--font-size-rem);
+    font-family: "Inter", sans-serif;
+    grid-row: 1;
+    grid-column: 1;
+    z-index: 1;
+    pointer-events: none;
+    transform: translateY(var(--font-size-rem)) translateX(calc(var(--font-size-rem) / 2)) scale(1);
+    background-color: white;
+    width: min-content;
+    height: min-content;
+    padding: 0 calc(var(--font-size-rem) / 2);
+    transition: transform var(--transition-length);
+    color: var(--shadow-color);
+  }
+
+  .input-wrapper {
+    display: grid;
+    grid-template-columns: 100%;
+    grid-template-rows: 100%;
+  }
+
+  input.component:not(:placeholder-shown) ~ .input-label {
+    transform: translateY(calc(var(--font-size-rem) * -0.5)) translateX(0) scale(0.8);
+  }
+
+  input.component:focus ~ .input-label {
+    transform: translateY(calc(var(--font-size-rem) * -0.5 + var(--shadow-y))) translateX(var(--shadow-x)) scale(0.8);
   }
 </style>
